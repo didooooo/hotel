ff55557ddc6c48dd77f57dc1c349542b00676ee4
Diyan Dimitrov <dimitrovdian330@gmail.com>
2024-08-16 16:17:21 +0300

    fixed rest-export

--- restexport/src/main/java/com.tinqinacademy.hotel.restexport/RestExportHotel.java
+++ restexport/src/main/java/com.tinqinacademy.hotel.restexport/RestExportHotel.java
@@ -4,13 +4,10 @@ import com.tinqinacademy.hotel.api.models.mapping.URLMapping;
 import com.tinqinacademy.hotel.api.models.operations.bookRoom.BookTheSpecificRoomInput;
 import com.tinqinacademy.hotel.api.models.operations.bookRoom.BookTheSpecificRoomOutput;
 import com.tinqinacademy.hotel.api.models.operations.checkRoom.CheckRoomAvailabilityOutput;
-import com.tinqinacademy.hotel.api.models.operations.getRegister.GetRegisterInput;
 import com.tinqinacademy.hotel.api.models.operations.getRegister.GetRegisterOutput;
-import com.tinqinacademy.hotel.api.models.operations.getRoomById.GetRoomByIDInput;
 import com.tinqinacademy.hotel.api.models.operations.getRoomById.GetRoomByIDOutput;
 import com.tinqinacademy.hotel.api.models.operations.registerVisitor.RegisterVisitorInput;
 import com.tinqinacademy.hotel.api.models.operations.registerVisitor.RegisterVisitorOutput;
-import com.tinqinacademy.hotel.api.models.operations.systemDelete.SystemDeleteRoomInput;
 import com.tinqinacademy.hotel.api.models.operations.systemDelete.SystemDeleteRoomOutput;
 import com.tinqinacademy.hotel.api.models.operations.systemPatch.SystemPatchRoomInput;
 import com.tinqinacademy.hotel.api.models.operations.systemPatch.SystemPatchRoomOutput;
@@ -18,90 +15,103 @@ import com.tinqinacademy.hotel.api.models.operations.systemPut.SystemPutRoomIdIn
 import com.tinqinacademy.hotel.api.models.operations.systemPut.SystemPutRoomOutput;
 import com.tinqinacademy.hotel.api.models.operations.systemRoom.SystemRoomInput;
 import com.tinqinacademy.hotel.api.models.operations.systemRoom.SystemRoomOutput;
-import com.tinqinacademy.hotel.api.models.operations.unbookRoom.UnbookRoomByIdInput;
 import com.tinqinacademy.hotel.api.models.operations.unbookRoom.UnbookRoomByIdOutput;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.UUID;
 
-@FeignClient("hotel-service")
+@Headers({"Content-Type: application/json"})
 public interface RestExportHotel {
     //Hotel
-    @GetMapping(URLMapping.CHECK_ROOM_AVAILABILITY)
-    CheckRoomAvailabilityOutput checkRoomAvailability(@RequestParam LocalDate startDate,
-                                                      @RequestParam LocalDate endDate,
-                                                      @RequestParam List<String> bedsInput,
-                                                      @RequestParam String bathroomType);
+    @RequestLine("GET /api/hotel/rooms?startDate={startDate}&endDate={endDate}&bedsInput={bedsInput}&bathroomType={bathroomType}")
+    //  @GetMapping(URLMapping.CHECK_ROOM_AVAILABILITY)
+    CheckRoomAvailabilityOutput checkRoomAvailability(@Param("startDate") LocalDate startDate,
+                                                      @Param("endDate") LocalDate endDate,
+                                                      @Param("bedsInput") List<String> bedsInput,
+                                                      @Param("bathroomType") String bathroomType);
 
-    @GetMapping(URLMapping.GET_ROOM_BY_ID)
-    GetRoomByIDOutput getRoomByIDOutput(@PathVariable String roomId);
+    @RequestLine("GET /api/hotel/{roomId}")
+        // @GetMapping(URLMapping.GET_ROOM_BY_ID)
+    GetRoomByIDOutput getRoomByIDOutput(@Param("roomId") String roomId);
 
-    @Operation(
-            summary = "book a room",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "booked")
-            }
-    )
-    @PostMapping(URLMapping.GET_ROOM_BY_ID)
-    BookTheSpecificRoomOutput bookSpecificRoom(@PathVariable("roomId") String roomId, @RequestBody BookTheSpecificRoomInput input);
+    //    @Operation(
+//            summary = "book a room",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "booked")
+//            }
+//    )
+    // @PostMapping(URLMapping.GET_ROOM_BY_ID)
+    @RequestLine("POST /api/hotel/{roomId}?input={input}")
+    BookTheSpecificRoomOutput bookSpecificRoom(@Param("roomId") String roomId, @Param("input") BookTheSpecificRoomInput input);
 
 
-    @DeleteMapping(URLMapping.UNBOOK_ROOM_BY_ID)
-    UnbookRoomByIdOutput unbookRoomById(@PathVariable("bookingId") String bookingId);
+    // @DeleteMapping(URLMapping.UNBOOK_ROOM_BY_ID)
+    @RequestLine("DELETE /api/hotel/{bookingId}")
+    UnbookRoomByIdOutput unbookRoomById(@Param("bookingId") String bookingId);
 
     //System
-    @PostMapping(URLMapping.REGISTER)
-    @Operation(
-            summary = "Register visitors"
-    )
-    RegisterVisitorOutput register(@RequestBody RegisterVisitorInput input);
 
-    @GetMapping(URLMapping.REGISTER)
-    GetRegisterOutput register(@RequestParam LocalDate startDate,
-                               @RequestParam LocalDate endDate,
-                               @RequestParam String roomNo,
-                               @RequestParam(required = false) String firstName,
-                               @RequestParam(required = false) String lastName,
-                               @RequestParam(required = false) String phoneNo,
-                               @RequestParam(required = false) String IDCardNumber,
-                               @RequestParam(required = false) String validity,
-                               @RequestParam(required = false) String authority,
-                               @RequestParam(required = false) String date
+    //    @PostMapping(URLMapping.REGISTER)
+//    @Operation(
+//            summary = "Register visitors"
+//    )
+    @RequestLine("POST /system/register?input={input}")
+    RegisterVisitorOutput register(@Param("input") RegisterVisitorInput input);
+
+    // @GetMapping(URLMapping.REGISTER)
+    @RequestLine("GET /system/register?startDate={startDate}&endDate={endDate}&roomNo={roomNo}" +
+            "&firstName={firstName}&lastName={lastName}&phoneNo={phoneNo}" +
+            "&IDCardNumber={IDCardNumber}&validity={validity}&" +
+            "authority={authority}&date={date}")
+    GetRegisterOutput register(@Param("startDate") LocalDate startDate,
+                               @Param("endDate") LocalDate endDate,
+                               @Param("roomNo") String roomNo,
+                               @Param("firstName") String firstName,
+                               @Param("lastName") String lastName,
+                               @Param("phoneNo") String phoneNo,
+                               @Param("IDCardNumber") String IDCardNumber,
+                               @Param("validity") String validity,
+                               @Param("authority") String authority,
+                               @Param("date") String date
     );
 
-    @DeleteMapping(URLMapping.SYSTEM_ROOM_BY_ID)
-    @Operation(summary = "Remove a room ",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "room removed")
-            }
-    )
-    SystemDeleteRoomOutput deleteRoom(@PathVariable String roomId);
+    @RequestLine("DELETE /system/room/{roomId}")
+//   // @DeleteMapping(URLMapping.SYSTEM_ROOM_BY_ID)
+//    @Operation(summary = "Remove a room ",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "room removed")
+//            }
+//    )
+    SystemDeleteRoomOutput deleteRoom(@Param("roomId") String roomId);
 
-    @PatchMapping(value = URLMapping.SYSTEM_ROOM_BY_ID, consumes = "application/json-patch+json")
-    SystemPatchRoomOutput patchRoom(@PathVariable String roomId, @RequestBody
+    @RequestLine("PATCH /system/room/{roomId}?input={input}")
+        //  @PatchMapping(value = URLMapping.SYSTEM_ROOM_BY_ID, consumes = "application/json-patch+json")
+    SystemPatchRoomOutput patchRoom(@Param("roomId") String roomId, @Param("input")
     SystemPatchRoomInput input);
 
-    @PutMapping(URLMapping.SYSTEM_ROOM_BY_ID)
-    @Operation(
-            summary = "edit a room",
-            responses = {
-                    @ApiResponse(responseCode = "403", description = "Forbidden")
-            }
-    )
-    SystemPutRoomOutput putRoom(@PathVariable String roomId, @RequestBody SystemPutRoomIdInput input);
+    @RequestLine("PUT /system/room/{roomId}?input={input}")
+        //@PutMapping(URLMapping.SYSTEM_ROOM_BY_ID)
+//    @Operation(
+//            summary = "edit a room",
+//            responses = {
+//                    @ApiResponse(responseCode = "403", description = "Forbidden")
+//            }
+//    )
+    SystemPutRoomOutput putRoom(@Param("roomId") String roomId, @Param("input") SystemPutRoomIdInput input);
 
-    @PostMapping(URLMapping.POST_ROOM)
-    @Operation(
-            summary = "add a room",
-            responses = {
-                    @ApiResponse(responseCode = "200", description = "Added a room")
-            }
-    )
-    SystemRoomOutput postRoom(@RequestBody SystemRoomInput input);
+    @RequestLine("POST /system/room?input={input}")
+//   // @PostMapping(URLMapping.POST_ROOM)
+//    @Operation(
+//            summary = "add a room",
+//            responses = {
+//                    @ApiResponse(responseCode = "200", description = "Added a room")
+//            }
+//    )
+    SystemRoomOutput postRoom(@Param("input") SystemRoomInput input);
 }
